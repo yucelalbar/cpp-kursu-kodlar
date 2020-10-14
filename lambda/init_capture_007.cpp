b1e17432ab0caed04826f4d62a4496fb9bf0b9d4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-14 16:46:51 +0300

    Create init_capture_007.cpp

---
+++ lambda/init_capture_007.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	int x = 9;
+	int y = 20;
+
+	auto f = [&x = x, y = x + 5]{ return ++x * y; };
+	std::cout << f() << "\n";
+	std::cout << "x = " << x << "\n";
+}
