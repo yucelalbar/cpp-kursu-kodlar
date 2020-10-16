84efd48cb9a0332ecbc2ed23e4def37fab382059
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-16 18:23:24 +0300

    Create count_01.cpp

---
+++ bitset/count_01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bitset>
+
+int main()
+{
+	using namespace std;
+	unsigned long uval;
+	cout << "bir tamsayi girin: ";
+	cin >> uval;
+	bitset<32> bs = uval;
+	cout << bs << "\n";
+	cout << "size = " << bs.size() << "\n";
+	cout << "set bit count = " << bs.count() << "\n";
+}
