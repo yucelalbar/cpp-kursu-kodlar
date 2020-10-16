caf7c243ff3ce8bfd51307fbf63ccc4219f5919d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-16 19:00:58 +0300

    Create bitset_equal_01.cpp

---
+++ bitset/bitset_equal_01.cpp
@@ -0,0 +1,20 @@
+#include <bitset>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	unsigned long x, y;
+	cout << "iki tamsayi girin: ";
+	cin >> x >> y;
+	bitset<16> bx{ x }, by{ y };
+
+	cout << bx << "\n" << by << "\n";
+
+	cout << boolalpha;
+	cout << (bx == by) << "\n";
+	cout << (bx != by) << "\n";
+
+	//bs < by; //yok boyle bir operator
+
+}
