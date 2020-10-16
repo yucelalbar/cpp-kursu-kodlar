7196fab3dacdb0943a471557a17027621f8345d3
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-16 17:54:20 +0300

    Create algo_param_01.cpp

---
+++ bind/algo_param_01.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <functional>
+#include "nutility.h"
+#include <algorithm>
+
+int main()
+{
+	using namespace std;
+	using namespace placeholders;
+
+	vector<int> ivec(1000);
+	generate(ivec.begin(), ivec.end(), [] {return rand() % 1000; });
+	int ival;
+
+	cout << "hangi degerden buyuk olanlar sayilsin: ";
+	cin >> ival;
+
+	cout << count_if(ivec.begin(), ivec.end(), [ival](int x) {return x > ival; }) << "\n";
+	cout << count_if(ivec.begin(), ivec.end(), bind(greater<int>{}, _1, ival)) << "\n";
+	cout << count_if(ivec.begin(), ivec.end(), bind(greater<>{}, _1, ival)); //transparent
+}
+
