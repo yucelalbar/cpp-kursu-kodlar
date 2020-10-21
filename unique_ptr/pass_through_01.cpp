e5ecda3add3f62960f006da0717318cd3d646596
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-21 17:50:22 +0300

    Create pass_through_01.cpp

---
+++ unique_ptr/pass_through_01.cpp
@@ -0,0 +1,22 @@
+#include <memory>
+#include "triple.h"
+
+
+//pass-through
+std::unique_ptr<Triple> 
+func(std::unique_ptr<Triple> uptr)
+{
+	std::cout << *uptr << "\n";
+	uptr->set(0, 0, 0);
+	///
+	return move(uptr);
+}
+
+int main()
+{
+	auto upx = std::make_unique<Triple>(1, 2, 3);
+
+	upx = func(move(upx));
+
+	std::cout << *upx << "\n";
+}
